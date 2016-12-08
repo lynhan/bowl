156c8a9763ba413618798f9aabc531974cb6a88e
lyn han <lynfhan@gmail.com>
2016-12-08 14:42:55 -0500

    place profile

--- src/container/Info.js
+++ src/container/Info.js
@@ -7,49 +7,82 @@ export default class Info extends Component {
         this.state = {
             pictureUrlStr: ""
         }
+        this.getPictureDownloadUrl = this.getPictureDownloadUrl.bind(this)
     }
 
 
     componentDidMount() {
+        this.getPictureDownloadUrl()
+    } // end componentDidMount
+
+
+    getPictureDownloadUrl() {
         let this_ = this
-        let storageRef = firebase.storage().ref();
-        let ref = storageRef.child(this.props.data.picture);
-        ref.getDownloadURL().then(function (url) {
-            console.log("URL", url)
-            this_.setState({ pictureUrlStr: url })
-        }).catch(function (error) {
-            switch (error.code) {
-                case 'storage/object_not_found':
-                    // File doesn't exist
-                    break;
-                case 'storage/unauthorized':
-                    // User doesn't have permission to access the object
-                    break;
-                case 'storage/canceled':
-                    // User canceled the upload
-                    break;
-                case 'storage/unknown':
-                    // Unknown error occurred, inspect the server response
-                    break;
-                default:
-                    console.log("omg")
-            }
-        });
-    }
+        if (this.props.data.picture) {
+            let storageRef = firebase.storage().ref();
+            let ref = storageRef.child(this.props.data.picture);
+            ref.getDownloadURL()
+            .then(function (url) {
+                this_.setState({ pictureUrlStr: url })
+            })
+            .catch(function (error) {
+                switch (error.code) {
+                    case 'storage/object_not_found':
+                        // File doesn't exist
+                        break;
+                    case 'storage/unauthorized':
+                        // User doesn't have permission to access the object
+                        break;
+                    case 'storage/canceled':
+                        // User canceled the upload
+                        break;
+                    case 'storage/unknown':
+                        // Unknown error occurred, inspect the server response
+                        break;
+                    default:
+                        console.log("omg")
+                }
+            }) // end catch
+        }  // end if
+    } // getPictureDownloadUrl
 
 
     render() {
         console.log('INFO ', this.props.data)
-        return (
-            <div className="info">
-                <div className="info-name">
-                    {this.props.data.name}
-                </div>
+
+        let picture 
+        if (this.state.pictureUrlStr) {
+            picture = (
                 <div className="info-picture">
                     <img src={this.state.pictureUrlStr}
                     alt="loading pic..."
                     width="200px"/>
                 </div>
+            )
+        }
+
+        let name
+        if (this.props.data.name) {
+            name = (
+                <div className="info-name">
+                    {this.props.data.name}
+                </div>
+            )
+        }
+
+        let summary
+        if (this.props.data.summary) {
+            summary = (
+                <div className="info-summary">
+                    {this.props.data.summary}
+                </div>
+            )
+        }
+        return (
+            <div className="info">
+                {picture}
+                {name}
+                {summary}
             </div>
         )
     }
--- src/routes/ProfilePlace/components/ProfilePlace.js
+++ src/routes/ProfilePlace/components/ProfilePlace.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import * as firebase from 'firebase/firebase-browser'
-// import Info from '../../../container/Info'
+import Info from '../../../container/Info'
 // import InfoList from '../../../container/InfoList'
 import { mapApiKey } from '../../../config'
 var axios = require('axios')
@@ -25,33 +25,31 @@ class ProfilePlace extends Component {
     observePlace() {
         let this_ = this
         let place_id = this.props.params.id
-        console.log('observePlaceid', place_id)
-        // first fetch and save latest data about place
+
         firebase
         .database()
         .ref('/place/' + this_.props.params.id)
         .once('value').then(function(snapshot) {
             let data = snapshot.val()
             if (data === null) {
-                console.log("NULL PLACE OOP")
                 let url = "https://maps.googleapis.com/maps/api/place/details/json?placeid=" + place_id + "&key=" + mapApiKey
-                // query map api
-                // TODO save findPlace query results in localStorage
-                // and look up here
-                // Make a request for a user with a given ID
-                axios.get(url)
+                
+                axios
+                .get(url)
                 .then(function (response) {
-                    console.log("RES", response);
                     let place = response.data.result
-                    let address = place.vicinity
-                    let website = place.website
-                    let phone = place.formatted_phone_number
+                    this_.setState({
+                        place: {
+                            name: place.name,
+                            summary: place.vicinity
+                        }
+                    })
                 })
                 .catch(function (error) {
-                    console.log("ERR", error);
-                });
+                    console.log("place info fetch error", error);
+                })
             } else {
-                console.log("WOW PLACE", data)
+
             }
         });
     }
@@ -60,7 +58,8 @@ class ProfilePlace extends Component {
     render() {
         return (
             <div className="profile-place">
-               { /* <Info data={this.state.place} /> */}
+               { /* data expects name, summary, picture /> */}
+               <Info data={this.state.place} />
                { /* <Infolist data={this.state.food} /> */}
             </div>
         )
